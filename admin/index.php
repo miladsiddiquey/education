974cab6ff2cec6af0598e055e9d75507f7c44af8
Milad Siddiquey <miladsiddiquey@gmail.com>
2024-06-30 10:51:14 +0600

    update

--- admin/index.php
+++ admin/index.php
@@ -1,41 +1,42 @@
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Education</title>
-    <!-- plugins:css -->
+
+    <!-- Include CSS files -->
     <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
+
+    <!-- Optional: Plugin CSS for specific page -->
     <link rel="stylesheet" href="assets/vendors/jvectormap/jquery-jvectormap.css">
     <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
     <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.theme.default.min.css">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
+
     <!-- Layout styles -->
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.png">
   </head>
   <body>
     <div class="container-scroller">
       <!-- partial:partials/_sidebar.php -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-          <a class="sidebar-brand brand-logo" href="index.php"><img src="assets/images/logo.svg" alt="logo" /></a>
-          <a class="sidebar-brand brand-logo-mini" href="index.php"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo" href="index.php"><img src="../../assets/images/logo.svg" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo-mini" href="index.php"><img src="../../assets/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <ul class="nav">
           <li class="nav-item profile">
             <div class="profile-desc">
               <div class="profile-pic">
                 <div class="count-indicator">
-                  <img class="img-xs rounded-circle " src="assets/images/faces/face15.jpg" alt="">
+                  <img class="img-xs rounded-circle " src="../../assets/images/faces/face15.jpg" alt="">
                   <span class="count bg-success"></span>
                 </div>
                 <div class="profile-name">
@@ -84,29 +85,14 @@
             <span class="nav-link">Navigation</span>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="index.php">
+            <a class="nav-link" href="../../index.php">
               <span class="menu-icon">
                 <i class="mdi mdi-speedometer"></i>
               </span>
               <span class="menu-title">Dashboard</span>
             </a>
           </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <span class="menu-icon">
-                <i class="mdi mdi-laptop"></i>
-              </span>
-              <span class="menu-title">Basic UI Elements</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.php">Buttons</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.php">Dropdowns</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.php">Typography</a></li>
-              </ul>
-            </div>
-          </li>
+
           <li class="nav-item menu-items">
             <a class="nav-link" href="pages/forms/basic_elements.php">
               <span class="menu-icon">
@@ -126,13 +112,13 @@
             </a>
             <div class="collapse" id="des_post">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../admin/pages/destination/add-destination.php">Add Destination</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../admin/pages/destination/list-destination.php">List Destination</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./pages/destination/add-destination.php">Add Destination</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./pages/destination/list-destination.php">List Destination</a></li>
               </ul>
             </div>
           </li>
-<!-- Team Members -->
-<li class="nav-item menu-items">
+          <!-- Team Members -->
+          <li class="nav-item menu-items">
             <a class="nav-link" data-toggle="collapse" href="#team-member" aria-expanded="false" aria-controls="ui-basic">
               <span class="menu-icon">
                 <i class="mdi mdi-laptop"></i>
@@ -142,14 +128,14 @@
             </a>
             <div class="collapse" id="team-member">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../admin/pages/team/add-team.php">Add Members</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../admin/pages/team/list-team.php">List Members</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./pages/team/add-team.php">Add Members</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./pages/team/list-team.php">List Members</a></li>
               </ul>
             </div>
           </li>
           <!-- client review  -->
-        <!-- Team Members -->
-<li class="nav-item menu-items">
+      
+          <li class="nav-item menu-items">
             <a class="nav-link" data-toggle="collapse" href="#cl_review" aria-expanded="false" aria-controls="ui-basic">
               <span class="menu-icon">
                 <i class="mdi mdi-laptop"></i>
@@ -175,8 +161,8 @@
             </a>
             <div class="collapse" id="college">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="../admin/pages/allcolleges/add-col.php">Add colleges</a></li>
-                <li class="nav-item"> <a class="nav-link" href="../admin/pages/allcolleges/list-col.php">List colleges</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./pages/allcolleges/add-col.php">Add colleges</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./pages/allcolleges/list-col.php">List colleges</a></li>
               </ul>
             </div>
           </li>
@@ -196,31 +182,6 @@
               </ul>
             </div>
           </li>
-
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="pages/tables/basic-table.php">
-              <span class="menu-icon">
-                <i class="mdi mdi-table-large"></i>
-              </span>
-              <span class="menu-title">Tables</span>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="pages/charts/chartjs.php">
-              <span class="menu-icon">
-                <i class="mdi mdi-chart-bar"></i>
-              </span>
-              <span class="menu-title">Charts</span>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="pages/icons/mdi.php">
-              <span class="menu-icon">
-                <i class="mdi mdi-contacts"></i>
-              </span>
-              <span class="menu-title">Icons</span>
-            </a>
-          </li>
           <li class="nav-item menu-items">
             <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
               <span class="menu-icon">
@@ -239,14 +200,6 @@
               </ul>
             </div>
           </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="http://www.bootstrapdash.com/demo/corona-free/jquery/documentation/documentation.php">
-              <span class="menu-icon">
-                <i class="mdi mdi-file-document-box"></i>
-              </span>
-              <span class="menu-title">Documentation</span>
-            </a>
-          </li>
         </ul>
       </nav>
       <!-- partial -->
@@ -254,7 +207,7 @@
         <!-- partial:partials/_navbar.php -->
         <nav class="navbar p-0 fixed-top d-flex flex-row">
           <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo-mini" href="index.php"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+            <a class="navbar-brand brand-logo-mini" href="index.php"><img src="../../assets/images/logo-mini.svg" alt="logo" /></a>
           </div>
           <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
